9a673b751f4728e3e4ca251bec2941c6cf3b3c66
Romuald Zdebskiy <zdebskis@mail.ru>
2018-06-05 14:03:55 +0100

    Add project files.

---
+++ Challenge2Team8/Models/RatingObject.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Newtonsoft.Json;
+using System.Threading.Tasks;
+
+namespace Challenge2Team8.Models
+{
+    public class RatingObject
+    {
+        [JsonProperty(PropertyName = "id")]
+        public string Id { get; set; }
+        [JsonProperty(PropertyName = "userId")]
+        public string UserId { get; set; }
+        [JsonProperty(PropertyName = "productId")]
+        public string ProductId { get; set; }
+        [JsonProperty(PropertyName = "timestamp")]
+        public string Timestamp { get; set; }
+        [JsonProperty(PropertyName = "locationName")]
+        public string LocationName { get; set; }
+        [JsonProperty(PropertyName = "rating")]
+        public int Rating { get; set; }
+        [JsonProperty(PropertyName = "userNotes")]
+        public string UserNotes { get; set; }
+    }
+}
